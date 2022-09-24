94b7d4d3d1889a6629767ccea8acf040562a3f2d
Salik47 <msalik32@gmail.com>
2022-09-25 03:54:28 +0530

    Bug Fix - File Deletion feature working

--- services/fileCleaner.js
+++ services/fileCleaner.js
@@ -1,18 +1,19 @@
-const connectDB = require("./config/db");
-const File = require("./models/file");
+require("dotenv").config({ path: "../.env" });
+const connectDB = require("../config/db");
+const File = require("../models/file");
 const fs = require("fs");
 
 connectDB();
 
 // Get all records older than 24 hours
-fetchData = async () => {
+let fetchData = async () => {
   const files = await File.find({
     createdAt: { $lt: new Date(Date.now() - 24 * 60 * 60 * 1000) },
   });
   if (files.length) {
     for (const file of files) {
       try {
-        fs.unlinkSync(file.path);
+        fs.unlinkSync("../" + file.path);
         await file.remove();
         console.log(`successfully deleted ${file.filename}`);
       } catch (err) {
