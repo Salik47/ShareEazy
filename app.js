d6d6ae8738bfe72213429fd8e84b0457c62a11b9
Salik47 <msalik32@gmail.com>
2022-09-25 03:52:42 +0530

    removed cors

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
 const connectDB = require("./config/db");
 require("dotenv").config;
 
@@ -13,21 +12,20 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 // Middlewares
-app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 if (process.env.NODE_ENV === "DEV") {
-	const morgan = require("morgan");
-	app.use(morgan("tiny"));
+  const morgan = require("morgan");
+  app.use(morgan("tiny"));
 }
 
 // Routes
 app.get("/", (_req, res) => {
-	res.sendFile("index.html", { root: path.join(__dirname, "public") });
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
 app.use("/api/files", require("./routes/files"));
 app.use("/api/pages", require("./routes/pages"));
 
 app.listen(PORT, () => {
-	console.log(`Listening on port: ${PORT}`);
+  console.log(`Listening on port: ${PORT}`);
 });
